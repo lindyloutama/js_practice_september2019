6ff41c882ffadd2198939385df635e87cfdbde50
lindyloutama <ltamakaha@yahoo.com>
2019-10-09 14:02:05 +0100

    challenges completed countSheep, noOfBuses, MpostCode

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,27 +1,44 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+   return (sandwich.fillings);
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+   if(person.city === "Manchester"){
+     return true;
+   } else {
+     return false;
+   }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let busNo = people / 40;
+    return Math.ceil(busNo);
 }
 
 function countSheep(arr) {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+ if (arr === undefined) throw new Error("arr is required");
+  let baabaa = 0;
+   for(let key in arr){
+   if(arr[key] === "sheep"){
+    baabaa++;
+   }
+  } 
+ return baabaa;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-}
+  let count = person.address.postCode
+  let index = 0;
+      if(count.charAt(index) === "M" && person.address.city === "Manchester"){
+       return true;
+  } else {
+  return false;
+  }
+ }
 
 module.exports = {
   getFillings,
