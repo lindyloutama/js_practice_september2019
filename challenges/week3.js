25fde2d07894f26379cf9698152175df34fc902b
lindyloutama <ltamakaha@yahoo.com>
2019-10-17 08:39:18 +0100

    update week 3 exercises

--- challenges/week3.js
+++ challenges/week3.js
@@ -1,17 +1,30 @@
-function getSquares(nums) {
+function camelCaseWords(words) {
+  if (words === undefined) throw new Error("words is required");
+  const camel = [words[0]];
+   for(let i = 1; i < words.length; i++) {
+     const word = words[i];
+      const capital = word[0].toUpperCase() + word.slice(1);
+       camel.push(capital);
+   }
+   return camel.join("");
+}
+  function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  if (nums === []) {
-
-  }
-
-  function camelCaseWords(words) {
-    if (words === undefined) throw new Error("words is required");
-    return camelCaseWords.toCamelCase();
-  }
+    const squares = [];
+    for(let i = 0; i< nums.length; i++) {
+     const square = Math.pow(nums[i], 2);
+      squares.push(square);
+    }
+   return squares;
+}
 
   function getTotalSubjects(people) {
     if (people === undefined) throw new Error("people is required");
-    // Your code here!
+     const nosubject = [];
+     const subjects = people.subject;
+      for(let i = 0; i < people.subject.length; i++) {
+        
+      }
   }
 
   function checkIngredients(menu, ingredient) {
